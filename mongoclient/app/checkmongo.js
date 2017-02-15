eb121aae27df9b9f7023d924158ddf7cc89ac54a
Nikola Stjelja <nstjelja@gmail.com>
2017-02-15 17:13:58 +0100

    Added checkup script for the mongo db client

---
+++ mongoclient/app/checkmongo.js
@@ -0,0 +1,32 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://admin:pass@172.17.0.1:32000/admin';
+
+var count = 0;
+var clearIntervalToken = setInterval(function(){
+    MongoClient.connect(url, function(err, db) {
+        count+=1;
+        console.log("Calling " + count + " times");
+
+        if (err !=null && count >= 15){
+            console.log("Called enough times");
+            clearInterval(clearIntervalToken);
+            process.exit(1);
+            return;
+        }
+
+        if (err != null){
+            console.log("Not connected to the mongodb server");
+            return;
+        }
+
+        clearInterval(clearIntervalToken);
+        console.log("Connected correctly to server.");
+        db.close();    
+        process.exit(0);
+    });
+},30000);
+
+
+
+
