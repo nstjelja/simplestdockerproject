126a674ea411a71dda0a149c43c5b59b93755bb4
Nikola Stjelja <nstjelja@gmail.com>
2017-02-17 18:38:27 +0100

    Completed basic local dev file

---
+++ app/entrypoint.sh
@@ -0,0 +1,2 @@
+node ./node_modules/gulp/bin/gulp.js
+node index.js "$@"
--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,12 @@
+
 var args = process.argv;
 console.dir(args);
-if (args.length > 2) {
+if (args.length > 2){
     console.log("Passed command line arguments");
-    args.splice(0, 2);
-    args.forEach(function (item) {
+    args.splice(0,2);
+    args.forEach(function(item){
         console.log(item);
     });
 }
+
 console.log("HELLO WORLD");
